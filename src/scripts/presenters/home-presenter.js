194198b8e5840eb22507d2112b0ed7b4373fad15
Enggar Sila Majid <111524521+enggarsilamajid@users.noreply.github.com>
2026-04-28 02:37:36 +0000

    delete dummy

--- src/scripts/presenters/home-presenter.js
+++ src/scripts/presenters/home-presenter.js
@@ -19,16 +19,6 @@ export default class HomePresenter {
       const response = await API.getStories();
       let stories = response.listStory;
 
-      if (!stories || stories.length === 0) {
-        stories = [{
-          name: 'Dummy User',
-          description: 'Ini data dummy',
-          photoUrl: 'https://via.placeholder.com/150',
-          lat: -6.2,
-          lon: 106.8,
-        }];
-      }
-
       this._view.renderStories(stories);
 
       await new Promise((r) => requestAnimationFrame(r));
