8a569d4b634595e5ae44727c3971d5333ee65405
Enggar Sila Majid <111524521+enggarsilamajid@users.noreply.github.com>
2026-04-28 05:43:24 +0000

    update api.js

--- src/scripts/data/api.js
+++ src/scripts/data/api.js
@@ -42,6 +42,29 @@ const API = {
 
     return await response.json();
   },
+
+  async addStory({ description, photo, lat, lon }) {
+    const token = localStorage.getItem('token');
+
+    const formData = new FormData();
+    formData.append('description', description);
+    formData.append('photo', photo);
+
+    if (lat && lon) {
+      formData.append('lat', lat);
+      formData.append('lon', lon);
+    }
+
+    const response = await fetch(ENDPOINTS.STORIES, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${token}`, 
+      },
+      body: formData, 
+    });
+
+    return await response.json();
+  },
 };
 
 export default API;
